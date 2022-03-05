e3da9d7dd6c094190e0cca778cc6e6b0f06bf176
Trọng Tú Đinh <trongtudinh@Trongs-MacBook-Pro.local>
2022-03-05 09:20:55 +0700

    Update Topic 8

--- selenium/webdriver/Topic_00_Template.java
+++ selenium/webdriver/Topic_00_Template.java
@@ -1,16 +1,11 @@
 package webdriver;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
-
-public class Topic_02_Selenium_Locator {
+public class Topic_00_Template {
     WebDriver driver;
     String projectPath = System.getProperty("user.dir");
 
--- selenium/webdriver/Topic_02_Selenium_Locator.java
+++ selenium/webdriver/Topic_02_Selenium_Locator.java
@@ -1,7 +1,5 @@
 package webdriver;
 
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -10,7 +8,9 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class Topic_01_Check_Environment {
+import java.util.concurrent.TimeUnit;
+
+public class Topic_02_Selenium_Locator {
     WebDriver driver;
     String projectPath = System.getProperty("user.dir");
 
@@ -24,27 +24,60 @@ public class Topic_01_Check_Environment {
     }
 
     @Test
-    public void TC_01_ValidateCurrentUrl() {
-        // Login Page Url matching
-        String loginPageUrl = driver.getCurrentUrl();
-        Assert.assertEquals(loginPageUrl, "https://www.facebook.com/");
-    }
+    public void TC_01_() {
+        //Selenium has 8 locator type
+        // Id
+        driver.findElement(By.id("email")).sendKeys("test@abc.com");
 
-    @Test
-    public void TC_02_ValidatePageTitle() {
-        // Login Page title
-        String loginPageTitle = driver.getTitle();
-        Assert.assertEquals(loginPageTitle, "Facebook – log in or sign up");
-    }
+        // Class
+        driver.findElement(By.className("fb_logo")).isDisplayed();
 
-    @Test
-    public void TC_03_LoginFormDisplayed() {
-        // Login form displayed
-        Assert.assertTrue(driver.findElement(By.xpath("//form[@data-testid='royal_login_form']")).isDisplayed());
+        // Name
+        driver.findElement(By.name("email")).isDisplayed();
+
+        // Tagname
+        driver.findElement(By.tagName("a"));
+
+        // Link Text
+        driver.findElement(By.linkText("English (UK)"));
+
+        // Partial Link Text
+        driver.findElement(By.partialLinkText("English (UK)"));
+
+        // XPath
+        driver.findElement(By.xpath("//a"));
+        driver.findElement(By.xpath("//a[text()='English (UK)']"));
+        driver.findElement(By.xpath("//a[contains(text(), 'English (UK)')]"));
+        driver.findElement(By.xpath("//input[@id='email']"));
+        driver.findElement(By.xpath("//input[@name='email']"));
+        driver.findElement(By.xpath("//img[@class='fb_logo _8ilh img']"));
+        driver.findElement(By.xpath("//img[contains(@class, 'fb_logo')]"));
+        driver.findElement(By.xpath("//img[start-with(@class, 'fb_logo')]"));
+
+        // Css
+        // Find by id
+        driver.findElement(By.cssSelector("input[id='email']"));
+        driver.findElement(By.cssSelector("input#email"));
+        driver.findElement(By.cssSelector("#email"));
+
+        // Find by class
+        driver.findElement(By.cssSelector("img[class='fb_logo _8ilh img']"));
+        driver.findElement(By.cssSelector("img.fb_logo"));
+        driver.findElement(By.cssSelector(".fb_logo"));
+
+        // Find by name
+        driver.findElement(By.cssSelector("img[name='email']"));
+
+        driver.findElement(By.cssSelector("a"));
+
+        //Css don't work with text
+        driver.findElement(By.cssSelector("a[title='Vietnamese']"));
+        driver.findElement(By.cssSelector("a[title*='Vietnamese']"));
+        driver.findElement(By.cssSelector("a[onClick*='vi_VN']"));
     }
 
     @AfterClass
     public void afterClass() {
-        driver.quit();
+        //driver.quit();
     }
 }
